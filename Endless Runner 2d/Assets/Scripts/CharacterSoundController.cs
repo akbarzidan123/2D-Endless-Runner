b55ab1af93646b9e5b08289344e2a8aa940e633d
akbar zidan <akbarzidan123@gmail.com>
2021-09-14 20:47:05 +0700

    2D-Endless-Runner

---
+++ Endless Runner 2d/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    private AudioSource audioPlayer;
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+         audioPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 2D-Endless-Runner
+
+Cara bermain:
+Klick Kiri untuk lompat
+Tekan "R" saat game over untuk restart game
+Tekan "esc" saat game over untuk keluar dari game
